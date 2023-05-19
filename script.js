cb2ebda74134d8e762fea54b003ef78c632b1d9e
Dwima Basumatary <dwima1583@gmail.com>
2023-05-19 14:56:02 +0530

    add persistent theme preference

--- script.js
+++ script.js
@@ -7,22 +7,34 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-toggleSwitch.addEventListener('change', switchTheme)
+toggleSwitch.addEventListener('change', switchTheme);
 
-
-function switchTheme(event){
-    if(event.target.checked){
-        document.documentElement.setAttribute('data-theme', 'dark');
+function switchTheme(event) {
+    if (event.target.checked) {
+        window.localStorage.setItem('theme', 'dark');
         darkMode();
-    }else{
-        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        window.localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
+// preferred theme
+(() => {
+    const currentTheme = window.localStorage.getItem('theme');
+
+    if (currentTheme && currentTheme === 'dark') {
+        toggleSwitch.checked = 'true';
+        darkMode();
+    }
+})()
+
+
 // Dark mode
 
-function darkMode(){
+function darkMode() {
+    document.documentElement.setAttribute('data-theme', 'dark');
+
     toggleIcon.children[0].innerText = 'Dark Mode';
     toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
     changeImageTheme('dark');
@@ -30,7 +42,9 @@ function darkMode(){
 
 //Light mode
 
-function lightMode(){
+function lightMode() {
+    document.documentElement.setAttribute('data-theme', 'light');
+    
     toggleIcon.children[0].innerText = 'Light Mode';
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
     changeImageTheme('light');
@@ -38,8 +52,8 @@ function lightMode(){
 
 /* Helper */
 
-function changeImageTheme(theme){
-    image1.src  = `img/undraw_proud_coder_${theme}.svg`;
+function changeImageTheme(theme) {
+    image1.src = `img/undraw_proud_coder_${theme}.svg`;
     image2.src = `img/undraw_feeling_proud_${theme}.svg`;
     image3.src = `img/undraw_conceptual_idea_${theme}.svg`;
 }
